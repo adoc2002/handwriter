cffb3b7c457fa4c694d459ec6c5e1bbe4bcbe04a
b-esc <ben.escobar.ben@gmail.com>
2019-03-05 10:33:37 -0600

    fixed plumber.R for working with api, hopefully won't need too much work on the webapp side

--- plumber.R
+++ plumber.R
@@ -15,7 +15,7 @@ function(msg=""){
 #' also a reference on how to use the package
 #' @param img_name img to be processed
 #' @get /plumbFeatures
-function(img_name, debug = FALSE){
+plumbt = function(img_name, debug = FALSE){
   #dependencies, todo: add conditional to check if first time server init
   test_path = "/home/esc/git_repos/fall_18/work/handwriter_webapp/uploads/"
   dt_test_path = "/home/ben/git_repos/csafe/handwriter_webapp/uploads/"
@@ -30,8 +30,27 @@ function(img_name, debug = FALSE){
   img_thinned = thinImage(img_binary)
   img_processed = processHandwriting(img_thinned,dim(img_binary))
   cat('finished processing\n')
+  cat('update to character features.. enumerating now...\n');
   #rename to character
   img_features = extract_character_features(img_processed$graphemeList,dim(img_binary))
   cat('processing has been finished, should be trying to push to node server')
   list(img_features)
 }
+#    R List Appendage
+#    lm_rm_nodelist = list()
+#    for(i in 1:length(character)){
+#      cur = character[[i]]$nodesInGraph
+#      lm_rm_nodelist = append(lm_rm_nodelist,list(list(leftMost = cur[[1]],rightMost = cur[[length(cur)]])))
+#    }
+#    return(lm_rm_nodelist)
+#  }
+
+gatherFeatures = function(processed_image){
+  featureList = list()
+  for(i in 1:length(processed_image$letterList)){
+    cur = processed_image$letterList[[i]]$characterFeatures
+    featureList = append(featureList,list(cur))
+  }
+  return(featureList)
+}
+
